3c83cff51d401b74e3032975d6cf8191d8a5a3fe
llr104 <947491240@qq.com>
2020-06-01 21:48:19 +0800

    Update client.go

--- main/client.go
+++ main/client.go
@@ -14,23 +14,29 @@ func main() {
 	c := server.NewClient()
 	c.Start()
 
+	//添加kv
 	c.Put("test","test_value",0)
-	c.Put("test1","test1_value",0)
+	c.Put("test1/tttt","test1_value",0)
 	c.Put("test2","test2_value",20)
+
+	//删除kv
 	c.Del("test1")
 
 	v := c.Get("test2")
 	fmt.Printf("test2:%s\n", v)
 
+	//监听key值，发生变化回调通知
 	c.WatchKey("watch1", func(k string, v string, t cache.OpType) {
 		fmt.Printf("watch key:%s, value:%s, type:%d\n", k, v, t)
 	})
 
+	//key值发生变化回调通知
 	c.WatchKey("unwatch", func(k string, v string, t cache.OpType) {
 		fmt.Printf("watch key:%s, value:%s, type:%d\n", k, v, t)
 	})
 
-	time.Sleep(time.Second*10)
+	time.Sleep(time.Second*20)
+	//取消监听key值的变化
 	c.UnWatchKey("unwatch")
 
 	time.Sleep(time.Second*10)
